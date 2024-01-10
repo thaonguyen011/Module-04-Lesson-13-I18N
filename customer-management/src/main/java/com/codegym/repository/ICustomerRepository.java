a3989e295bdbe0513f10429ca945a109577d445a
thaonguyen011 <thanhthaohoa9999@gmail.com>
2024-01-10 15:13:37 +0700

    Add new file

---
+++ customer-management/src/main/java/com/codegym/repository/ICustomerRepository.java
@@ -0,0 +1,19 @@
+package com.codegym.repository;
+
+import com.codegym.model.Customer;
+import com.codegym.model.Province;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface ICustomerRepository extends CrudRepository<Customer, Long> {
+    Iterable<Customer> findAllByProvince(Province province);
+    Page<Customer> findAll(Pageable pageable);
+    Page<Customer> findAllByFirstNameContaining(Pageable pageable, String name);
+
+    @Query("select c from Customer c where c.id = :id")
+    Customer getCustomer(@Param("id") Long id);
+;}
+
